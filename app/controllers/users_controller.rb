b936cedeb6013bcac4dc550e6098d33f706d419b
brandonricharda <brandonricharda@gmail.com>
2021-05-01 18:18:50 -0400

    Removes linebreaks in preparation for proper styling

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,12 @@ class UsersController < ApplicationController
     def index
         @request = FriendRequest.new
     end
+
+    def show
+        @user = User.find(params[:id])
+        @posts = @user.posts
+        @like = Like.new
+        @post = Post.new
+        @comment = Comment.new
+    end
 end
