cd5b3ebf15ddd717bc9ab42ff61e13ead4732fee
brandonricharda <brandonricharda@gmail.com>
2021-05-09 14:56:47 -0400

    Creates seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+users = ["Brandon Austin", "Steve Jobs", "Bill Gates", "Elon Musk", "Barack Obama", "Joe Biden", "Donald Trump", "Serena Williams", "Katy Perry", "Adam Sandler"]
+
+users_info = {}
+
+users.each do |name|
+    users_info[name] = "#{name.split(" ")[0].downcase}@gmail.com"
+end
+
+users_info.each do |full_name, email|
+    User.create(:fname => full_name.split(" ")[0], :lname => full_name.split(" ")[1], :email => email, :password => "#{full_name.split(" ")[0]}123")
+end
+
+User.all.each do |user|
+    user.posts.create(:content => "This is what a post from #{user.name} would look like!")
+end
+
+User.all.each_slice(5).to_a[0].each do |user|
+    User.all.each_slice(5).to_a[1].each do |friend|
+        user.sent_requests.create(:recipient_id => friend.id, :status => true)
+    end
+end
